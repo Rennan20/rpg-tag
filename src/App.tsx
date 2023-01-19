e1b85af1b28898061f46dee22d11884f1bf83b0f
Rennan Silva <jrennan14@gmail.com>
2023-01-19 11:49:42 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import * as C from "./App.styles";
+import { Character } from "./components/Character";
+import { useCharacter } from "./hooks/useCharacter";
+import { Character2 } from "./components/Character2";
 
-function App() {
+const App = () => {
+  const char = useCharacter("Jane");
+  const char2 = useCharacter("John");
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+  }, []);
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    switch (e.code) {
+      case "KeyA":
+        char.moveLeft();
+        break;
+      case "KeyW":
+        char.moveUp();
+        break;
+      case "KeyD":
+        char.moveRight();
+        break;
+      case "KeyS":
+        char.moveDown();
+        break;
+    }
+    switch (e.code) {
+      case "ArrowLeft":
+        char2.moveLeft();
+        break;
+      case "ArrowUp":
+        char2.moveUp();
+        break;
+      case "ArrowRight":
+        char2.moveRight();
+        break;
+      case "ArrowDown":
+        char2.moveDown();
+        break;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Map>
+        <Character x={char.x} y={char.y} side={char.side} name={char.name} />
+        <Character2
+          x={char2.x}
+          y={char2.y}
+          side={char2.side}
+          name={char2.name}
+        />
+      </C.Map>
+      <footer>
+        John se move utilizando as setas <br />
+        Jane se move utilizando as teclas "AWSD"
+      </footer>
+    </C.Container>
   );
-}
+};
 
 export default App;
---
+++ src/hooks/useCharacter.ts
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { CharacterSides } from "../types/CharacterSides";
+import { mapaSpots } from "../data/mapaSpots";
+
+export const useCharacter = (propName: string) => {
+  const [name, setName] = useState(propName);
+  const [pos, setPos] = useState({ x: 3, y: 5 });
+  const [pos2, setPos2] = useState({ x: 5, y: 5 });
+
+  const [side, setSide] = useState<CharacterSides>("down");
+
+  const moveLeft = () => {
+    setPos((pos) => ({
+      x: canMove(pos.x - 1, pos.y) ? pos.x - 1 : pos.x,
+      y: pos.y,
+    }));
+    setSide("left");
+  };
+  const moveRight = () => {
+    setPos((pos) => ({
+      x: canMove(pos.x + 1, pos.y) ? pos.x + 1 : pos.x,
+      y: pos.y,
+    }));
+    setSide("right");
+  };
+  const moveDown = () => {
+    setPos((pos) => ({
+      x: pos.x,
+      y: canMove(pos.x, pos.y + 1) ? pos.y + 1 : pos.y,
+    }));
+    setSide("down");
+  };
+  const moveUp = () => {
+    setPos((pos) => ({
+      x: pos.x,
+      y: canMove(pos.x, pos.y - 1) ? pos.y - 1 : pos.y,
+    }));
+    setSide("up");
+  };
+
+  const canMove = (x: number, y: number) => {
+    if (mapaSpots[y] !== undefined && mapaSpots[y][x] !== undefined) {
+      return mapaSpots[y][x] === 1;
+    }
+    return false;
+  };
+
+  return {
+    name,
+    x: pos.x,
+    y: pos.y,
+    side,
+    moveLeft,
+    moveRight,
+    moveDown,
+    moveUp,
+  };
+};
